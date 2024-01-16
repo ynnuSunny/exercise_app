9c523985420345eafd6f5e638cfbc4d25327d720
ynnuSunny <sunnysutradhar38@gmail.com>
2024-01-16 09:48:00 +0600

    update details exercise

--- lib/src/screens/exercise_details.dart
+++ lib/src/screens/exercise_details.dart
@@ -1,6 +1,7 @@
 import 'package:exercise_app/src/models/exercise_model.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:lottie/lottie.dart';
 
 class ExerciseDetailsScreen extends StatefulWidget {
   ExerciseModel exerciseModel;
@@ -14,11 +15,10 @@ class _ExerciseDetailsScreenState extends State<ExerciseDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Excercise Details"),
-      ),
-      body: TimerWidget()
-    );
+        appBar: AppBar(
+          title: Text("Excercise Details"),
+        ),
+        body: TimerWidget());
   }
 }
 
@@ -30,7 +30,7 @@ class TimerWidget extends StatefulWidget {
 class _TimerWidgetState extends State<TimerWidget> {
   int _seconds = 0;
   late Timer _timer;
-  bool _isPaused = false;
+  bool _isPaused = true;
 
   @override
   void initState() {
@@ -58,8 +58,17 @@ class _TimerWidgetState extends State<TimerWidget> {
 
   void _resetTimer() {
     setState(() {
+      _isPaused = true;
       _seconds = 0;
     });
+  }
+
+  String _getTime(int second) {
+    // int minute = 0;
+    // if(second>=60 && second%60==0){
+    //   minute = second~/60;
+    // }
+    return "${second ~/ 60} : ${second % 60}";
   }
 
   @override
@@ -68,29 +77,49 @@ class _TimerWidgetState extends State<TimerWidget> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'Time: $_seconds seconds',
-            style: TextStyle(fontSize: 24.0),
+          Expanded(
+              flex: 2,
+              child: Container(
+                child: Lottie.asset("assets/lottie/exercise.json"),
+              )),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: 150.0, // Adjust the width as needed
+              height: 150.0, // Adjust the height as needed
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+              ),
+              child: Center(
+                child: Text(
+                  _getTime(_seconds),
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+            ),
           ),
-          SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: _startTimer,
-                child: Text('Start'),
-              ),
-              SizedBox(width: 20.0),
-              ElevatedButton(
-                onPressed: _stopTimer,
-                child: Text('Pause'),
-              ),
-              SizedBox(width: 20.0),
-              ElevatedButton(
-                onPressed: _resetTimer,
-                child: Text('Reset'),
-              ),
-            ],
+          // SizedBox(height: 20.0),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _startTimer,
+                  child: Text('Start'),
+                ),
+                SizedBox(width: 20.0),
+                ElevatedButton(
+                  onPressed: _stopTimer,
+                  child: Text('Pause'),
+                ),
+                SizedBox(width: 20.0),
+                ElevatedButton(
+                  onPressed: _resetTimer,
+                  child: Text('Reset'),
+                ),
+              ],
+            ),
           ),
         ],
       ),
